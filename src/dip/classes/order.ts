2055ee5e85d1e6fee5b8161a7755fff3afe97816
Lucas Mello dos Santos <melloluc02@gmail.com>
2023-07-15 23:14:18 -0300

    Dependency Inversion Principle

---
+++ src/dip/classes/order.ts
@@ -0,0 +1,38 @@
+import { OrderStatus } from './interfaces/order-status';
+import { CustomerOrder } from './interfaces/customer-protocol';
+import { ShoppingCartProtocol } from './interfaces/shopping-cart-protocol';
+import { MessagingProtocol } from './interfaces/messaging-protocol';
+import { PersistencyProtocol } from './interfaces/persistency-protocol';
+
+export class Order {
+  constructor(
+    private readonly cart: ShoppingCartProtocol,
+    private readonly messaging: MessagingProtocol,
+    private readonly persistency: PersistencyProtocol,
+    private readonly customer: CustomerOrder,
+  ) {}
+
+  private _orderStatus: OrderStatus = 'open';
+
+  get orderStatus(): Readonly<OrderStatus> {
+    return this._orderStatus;
+  }
+
+  checkout(): void {
+    if (this.cart.isEmpty()) {
+      console.log('Seu carrinho está vazio');
+      return;
+    }
+
+    this._orderStatus = 'closed';
+    this.messaging.sendMessage(
+      `Seu pedido com total de ${this.cart.total()} foi recebido.`,
+    );
+
+    console.log('Cliente: ' + this.customer.getName());
+    console.log('Identificação: ' + this.customer.getIDN());
+
+    this.persistency.saveOrder();
+    this.cart.clear();
+  }
+}
