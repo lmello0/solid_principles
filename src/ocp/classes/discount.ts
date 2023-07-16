8226e4a41f888bf68e0bf732ca0a723f97c74efc
Lucas Mello dos Santos <melloluc02@gmail.com>
2023-07-15 22:35:47 -0300

    Open/Closed Principle

---
+++ src/ocp/classes/discount.ts
@@ -0,0 +1,11 @@
+export abstract class Discount {
+  protected discount = 0;
+
+  calculate(price: number): number {
+    return price - price * this.discount;
+  }
+}
+
+export class FiftyPercentDiscount extends Discount {
+  protected discount = 0.5;
+}
